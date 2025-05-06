35cdbcf766152090d1cc8ab1cc5ab35bec06a1ba
Toth Alexandra <atoth1433@gmail.com>
2025-05-06 17:26:04 +0300

    ,

--- src/test/java/pizzashop/service/PaymentSRE.java
+++ src/test/java/pizzashop/service/PaymentSRE.java
@@ -8,8 +8,6 @@ import pizzashop.repository.MenuRepository;
 import pizzashop.repository.PaymentRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class PaymentSRE {
 
