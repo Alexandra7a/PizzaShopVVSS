7357210839e09025e7e2ae172d429e896bec78af
Toth Alexandra <atoth1433@gmail.com>
2025-05-15 12:33:02 +0300

    pom

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,13 +5,11 @@ module pizzashop {
     requires java.desktop;
 
     opens pizzashop.model to javafx.base;
-    opens pizzashop.controller to javafx.fxml;
-    opens pizzashop to javafx.fxml;
-    opens pizzashop.service to javafx.base, org.junit.jupiter.api;
-
     exports pizzashop.model;
-    exports pizzashop.controller;
-    exports pizzashop.service;
+    opens pizzashop to javafx.fxml;
     exports pizzashop;
+    opens pizzashop.controller to javafx.fxml;
+    exports pizzashop.controller;
+    opens pizzashop.service to org.junit.platform.commons;
 
 }
